8816a49fd375c184b5624165ccd8659da3961168
polacoluan <polacoluan@gmail.com>
2022-05-18 15:31:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+echo "
+<center><a href='www.econeteditora.com.br' target='blank'<button type='button' class='btn btn-secondary'><h1>Insta Gabriela</h1></button></a></center>";
+?> 
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+	<title>.:Hora do Lanche:.</title>
+</head>
+<body>
+<h1>Hora do lanche</h1>
+
+<table class="table table-bordered">
+	<thead>
+		<th>Tabela de precos</th>
+	</thead>
+	<tr>
+		<td>Pastel</td>
+		<td>2.50</td>
+	</tr>
+</table>
+
+</body>
+</html>
